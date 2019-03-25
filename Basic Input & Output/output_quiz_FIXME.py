14b9e51426ec63720e3be46ce30b5e5cac49e480
lvolkmann <landonvolkmann@gmail.com>
2019-03-25 12:14:54 -0500

    Finish input & output folder

---
+++ Basic Input & Output/output_quiz_FIXME.py
@@ -0,0 +1,14 @@
+"""
+Print the following using the function print()
+1.	A single newline
+2.	2 newlines using a single statement
+3.	The values "cat" and "dog" separated by a space
+4.	The values "cat" and "dog" without a space
+5.	The value "hello" that doesn't end in a newline
+6.	The value "hello" that ends with a "!" via the print()
+7.	The value "Now that's Italian" using single ticks
+8.	The integer 9 and the value "lives" separated by a space
+9.	The value found in the variable name
+"""
+# Test name for question 9
+name = "Gregory"

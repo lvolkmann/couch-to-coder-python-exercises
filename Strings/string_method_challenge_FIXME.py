d23e1a0f4a8aa01105b7d85a7b903228515a44ed
lvolkmann <landonvolkmann@gmail.com>
2019-05-10 12:46:35 -0500

    Update strings folder

---
+++ Strings/string_method_challenge_FIXME.py
@@ -0,0 +1,8 @@
+def remove2(str_lst: list, sub: str) -> tuple:
+    """Write a function that accepts a list of strings and another string that removes that string from the list
+without list iteration or list methods (aka only string methods).
+It should return a tuple with the updated string, number of deletions, and the indexes of the deleted values."""
+    pass
+
+        
+        

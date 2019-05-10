fae2b7f0521e3e9793aa4f7a937db27db20f9962
lvolkmann <landonvolkmann@gmail.com>
2019-05-10 12:43:37 -0500

    Update files folder

--- Files/password_generator.py
+++ Files/password_generator.py
@@ -1,13 +1,20 @@
-#open files
+"""
+You've lost your password but you know the common elements you use that you saved in passwords.txt
+    - Write a program to create every combination of password elements
+    - Output these combinations to another file
+    - NOTE: every password has three elements separated by commas name, symbol, number
+"""
+
+# open files
 fh = open("passwords.txt", "r")
 out = open("Output.txt", "w")
 
-#initialize blank lists
+# initialize blank lists
 element1 = []
 element2 = []
 element3 = []
 
-#get elements
+# get elements
 for line in fh:
     line = line.strip('\n')
     elements = line.split(sep = ',')
@@ -15,13 +22,13 @@ for line in fh:
     element2.append(elements[1])
     element3.append(elements[2])
     
-#combine elements 
+# combine elements 
 for a in element1:
     for b in element2:
         for c in element3:
             #write elements
             out.write(a+b+c+"\n")
             
-#close files
+# close files
 fh.close()
 out.close()
---
+++ Files/password_generator_FIXME.py
@@ -0,0 +1,20 @@
+"""
+You've lost your password but you know the common elements you use that you saved in passwords.txt
+    - Write a program to create every combination of password elements
+    - Output these combinations to another file
+    - NOTE: every password has three elements separated by commas name, symbol, number
+"""
+
+# open files
+
+
+# initialize blank lists
+
+
+# get elements
+
+    
+# combine elements 
+
+            
+# close files
